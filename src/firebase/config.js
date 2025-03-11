8f2cf2e9a9212a83fcd90262e2416d6e7bcaafac
kliuverTsk <kliuvertfigueroa@gmail.com>
2025-03-11 09:22:34 -0300

    Move Firebase config to environment variables

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA0fiGGAPDwRC-tTvQLzP_p1ckM3qWBie8",
-  authDomain: "tienda-online-45d1c.firebaseapp.com",
-  projectId: "tienda-online-45d1c",
-  storageBucket: "tienda-online-45d1c.firebasestorage.app",
-  messagingSenderId: "99161079417",
-  appId: "1:99161079417:web:5fca3cb0ae9b3525921ea8"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
